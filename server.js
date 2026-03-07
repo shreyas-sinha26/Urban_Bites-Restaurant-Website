376a50d362ab9108ecf0fc79cf0a76f85307adb6
shreyas-sinha26 <shreyas.sinha26@gmail.com>
2026-03-07 23:42:14 +0530

    changes

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ app.post('/reservation', async (req, res) => {
 });
 
 // --- Start Server ---
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
